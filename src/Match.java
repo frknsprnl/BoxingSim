0e8eaafd67d517bc187a3bdd4d51dc31b06fe286
Furkan <frknsprnl@hotmail.com>
2022-04-21 11:32:29 +0300

    Fighter & Match classes created

---
+++ src/Match.java
@@ -0,0 +1,13 @@
+public class Match {
+    Fighter f1;
+    Fighter f2;
+    int minWeight;
+    int maxWeight;
+
+    Match(Fighter f1, Fighter f2, int minWeight, int maxWeight) {
+        this.f1 = f1;
+        this.f2 = f2;
+        this.minWeight = minWeight;
+        this.maxWeight = maxWeight;
+    }
+}
